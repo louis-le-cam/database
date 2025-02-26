2ef21105dd5960f6bf7f18c0cacc2059cee66043
Louis Le Cam <louislecam25@gmail.com>
2025-02-26 18:18:52 +0100

    rename 'from_path' module to 'path' since it does not only contains the FromPath trait

--- src/expression/mod.rs
+++ src/expression/mod.rs
@@ -1,12 +1,19 @@
 mod expression;
-mod from_path;
 mod impls;
 mod node;
 mod operators;
+mod path;
 
 pub use self::{
     expression::Expression,
-    from_path::{
+    node::{expression_discriminant, ExpressionNode},
+    operators::{
+        And, AndExpression, Chain, ChainExpression, EqualExpression, Filter, FilterExpression,
+        FuseExpression, GetExpression, Int128Equal, Int16Equal, Int32Equal, Int64Equal, Int8Equal,
+        MapVariantExpression, OptionOperators, Set, SetExpression, StringEqual, Uint128Equal,
+        Uint16Equal, Uint32Equal, Uint64Equal, Uint8Equal, VecGet,
+    },
+    path::{
         BoolExpression, DurationExpression, Float32Expression, Float64Expression, FromPath,
         HashMapExpression, HashSetExpression, Int128Expression, Int16Expression, Int32Expression,
         Int64Expression, Int8Expression, OptionExpression, OptionMappedExpression,
@@ -16,12 +23,5 @@ pub use self::{
         TupleExpression5, TupleExpression6, TupleExpression7, TupleExpression8, TupleExpression9,
         Uint128Expression, Uint16Expression, Uint32Expression, Uint64Expression, Uint8Expression,
         UnitExpression, VecExpression,
-    },
-    node::{expression_discriminant, ExpressionNode},
-    operators::{
-        And, AndExpression, Chain, ChainExpression, EqualExpression, Filter, FilterExpression,
-        FuseExpression, GetExpression, Int128Equal, Int16Equal, Int32Equal, Int64Equal, Int8Equal,
-        MapVariantExpression, OptionOperators, Set, SetExpression, StringEqual, Uint128Equal,
-        Uint16Equal, Uint32Equal, Uint64Equal, Uint8Equal, VecGet,
     },
 };
